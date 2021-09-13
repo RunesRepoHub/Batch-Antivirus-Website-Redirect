0f04385bd067453f9a2dba2030f07018f047e21e
rune <christian.rune1998@gmail.com>
2021-09-13 18:23:17 +0200

    Update to run bat

--- Run BAWR-V1.0.0.bat
+++ Run BAWR-V1.0.0.bat
@@ -1,1 +1,24 @@
+@ECHO OFF
+:start
+SET choice=
+SET /p choice=Do something? [N]:
+IF NOT '%choice%'=='' SET choice=%choice:~0,1%
+IF '%choice%'=='Y' GOTO yes
+IF '%choice%'=='y' GOTO yes
+IF '%choice%'=='N' GOTO no
+IF '%choice%'=='n' GOTO no
+IF '%choice%'=='' GOTO no
+ECHO "%choice%" is not valid
+ECHO.
+GOTO start
+
+:no
+ECHO Do all of the no things here!
+PAUSE
+EXIT
+
+:yes
+ECHO Do all of the yes things here!
 start cmd.exe /k notepad "C:\Windows\System32\drivers\etc folder"
+PAUSE
+EXIT
