483c88a68483679b5419a62253ad284003527251
rune <christian.rune1998@gmail.com>
2021-09-14 17:32:36 +0200

    Update to edit hosts file
    Minor fix

--- BAWR-functions/edit-hosts-file.bat
+++ BAWR-functions/edit-hosts-file.bat
@@ -52,7 +52,6 @@ http://lightforcefinder.com/search.php?q=medical+symptoms+checker
 http://expendablesearch.com/search.php?q=testosterone+cypionate
 http://expendablesearch.com http://et-phone.co.kr/ bnicsl.no-ip.org:443
 fantasy.legend.rocks:7777  irc.byroe.net:6667 mafia.bot.nu:4444 199.114.220.147:8443
-
 mercetruck.com.br ngipharmacy.com www.lizsol.com diamondsgrouphr.com artoftheruse.com
 bluecoatict.co.uk lopesdedetizacao.com.br licmirtabraier.com.ar bhardwajcarriers.com
 cartomantealex.com danlaid.com.au powerful-spark-age.glitch.me www.online-et.com
@@ -63,7 +62,7 @@ www.felippealfredo.com elfin-adaptive-recorder.glitch.me www.designmycakes.com
 colegiolecole.edu.mx boltrucks.us centraslcourierllc.com geekvb.in janglo.net
 bookedandboarding.com www.azhomes.com besthomeworkhelp.org kongrespdgi.com
 sepehrparmis.com kerfoo.com itmagazin.info designmycakes.com www.designmycakes.com
-petbijoux.com.br www.facepooc.com cloud9ine.ru promcuscotravel.com is.facviews.com
+petbijoux.com.br www.facepooc.com cloud9ine.ru promcuscotravel.com is.facviews.com 
 www.voolenstudios.com wpcccricket.co.za gobbojr.com.br dejavumedspaaz.com)
 
 ::Set the ip of the domains you set in the list above
@@ -135,55 +134,55 @@ set fantasy.legend.rocks:7777=127.0.0.1
 set irc.byroe.net:6667=127.0.0.1
 set mafia.bot.nu:4444=127.0.0.1
 set 199.114.220.147:8443=127.0.0.1
-set mercetruck.com.br=127.0.0.1 
-set ngipharmacy.com=127.0.0.1 
-set www.lizsol.com=127.0.0.1 
-set diamondsgrouphr.com=127.0.0.1 
+set mercetruck.com.br=127.0.0.1
+set ngipharmacy.com=127.0.0.1
+set www.lizsol.com=127.0.0.1
+set diamondsgrouphr.com=127.0.0.1
 set artoftheruse.com=127.0.0.1
-set bluecoatict.co.uk=127.0.0.1 
-set lopesdedetizacao.com.br=127.0.0.1 
-set licmirtabraier.com.ar=127.0.0.1 
+set bluecoatict.co.uk=127.0.0.1
+set lopesdedetizacao.com.br=127.0.0.1
+set licmirtabraier.com.ar=127.0.0.1
 set bhardwajcarriers.com=127.0.0.1
-set cartomantealex.com=127.0.0.1 
-set danlaid.com.au=127.0.0.1 
-set powerful-spark-age.glitch.me=127.0.0.1 
+set cartomantealex.com=127.0.0.1
+set danlaid.com.au=127.0.0.1
+set powerful-spark-age.glitch.me=127.0.0.1
 set www.online-et.com=127.0.0.1
-set mbaudiovisual.com.br=127.0.0.1 
-set www.reservation-ouicar.com=127.0.0.1 
-set workdefender.com=127.0.0.1 
+set mbaudiovisual.com.br=127.0.0.1
+set www.reservation-ouicar.com=127.0.0.1
+set workdefender.com=127.0.0.1
 set arvnorton.com=127.0.0.1
-set habuprocess.com=127.0.0.1 
-set www.shebasecurity.com=127.0.0.1 
-set laborerpattern.com=127.0.0.1 
+set habuprocess.com=127.0.0.1
+set www.shebasecurity.com=127.0.0.1
+set laborerpattern.com=127.0.0.1
 set dallascarcrashlawyer.com=127.0.0.1
-set mail.reservation-ouicar.com=127.0.0.1 
-set styleandharmony.com.au=127.0.0.1 
+set mail.reservation-ouicar.com=127.0.0.1
+set styleandharmony.com.au=127.0.0.1
 set dev.klinikmatanusantara.com=127.0.0.1
-set www.felippealfredo.com=127.0.0.1 
-set elfin-adaptive-recorder.glitch.me=127.0.0.1 
+set www.felippealfredo.com=127.0.0.1
+set elfin-adaptive-recorder.glitch.me=127.0.0.1
 set www.designmycakes.com=127.0.0.1
-set colegiolecole.edu.mx=127.0.0.1 
-set boltrucks.us=127.0.0.1 
-set centraslcourierllc.com=127.0.0.1 
-set geekvb.in=127.0.0.1 
+set colegiolecole.edu.mx=127.0.0.1
+set boltrucks.us=127.0.0.1
+set centraslcourierllc.com=127.0.0.1
+set geekvb.in=127.0.0.1
 set janglo.net=127.0.0.1
-set bookedandboarding.com=127.0.0.1 
-set www.azhomes.com=127.0.0.1 
-set besthomeworkhelp.org=127.0.0.1 
+set bookedandboarding.com=127.0.0.1
+set www.azhomes.com=127.0.0.1
+set besthomeworkhelp.org=127.0.0.1
 set kongrespdgi.com=127.0.0.1
-set sepehrparmis.com=127.0.0.1 
-set kerfoo.com=127.0.0.1 
-set itmagazin.info=127.0.0.1 
-set designmycakes.com=127.0.0.1 
+set sepehrparmis.com=127.0.0.1
+set kerfoo.com=127.0.0.1
+set itmagazin.info=127.0.0.1
+set designmycakes.com=127.0.0.1
 set www.designmycakes.com=127.0.0.1
-set petbijoux.com.br=127.0.0.1 
-set www.facepooc.com=127.0.0.1 
-set cloud9ine.ru=127.0.0.1 
-set promcuscotravel.com=127.0.0.1 
+set petbijoux.com.br=127.0.0.1
+set www.facepooc.com=127.0.0.1
+set cloud9ine.ru=127.0.0.1
+set promcuscotravel.com=127.0.0.1
 set is.facviews.com=127.0.0.1
-set www.voolenstudios.com=127.0.0.1 
-set wpcccricket.co.za=127.0.0.1 
-set gobbojr.com.br=127.0.0.1 
+set www.voolenstudios.com=127.0.0.1
+set wpcccricket.co.za=127.0.0.1
+set gobbojr.com.br=127.0.0.1
 set dejavumedspaaz.com=127.0.0.1
 
 
