20e6770c2792c31387128e9de0f608ddb6c952ea
rune <christian.rune1998@gmail.com>
2021-09-14 14:55:52 +0200

    Update to run bat
    New looks and code

--- Run BAWR-V1.0.0.bat
+++ Run BAWR-V1.0.0.bat
@@ -1,42 +1,46 @@
-@ECHO OFF
+call "c:\Users\%Username%\Desktop\BAWR-functions\move-edit-host-file.bat"
+call "c:\Windows\System32\drivers\etc\edit-hosts-file.bat"
 
-ECHO 1.Update-Host-Files
-ECHO 2.Shutdown
-ECHO 3.Close all Windows
-ECHO 4.Log off
-ECHO 5.Switch User
-ECHO.
 
-CHOICE /C 12345 /M "Enter your choice:"
 
-:: Note - list ERRORLEVELS in decreasing order
-IF ERRORLEVEL 5 GOTO SwitchUser
-IF ERRORLEVEL 4 GOTO Logoff
-IF ERRORLEVEL 3 GOTO CloseAllWindows
-IF ERRORLEVEL 2 GOTO Shutdown
-IF ERRORLEVEL 1 GOTO Update-Host-Files
 
-:Update-Host-Files
+echo off
+:begin
+echo Select a task:
+echo =============
+echo -
+echo 1) Update Host Files
+echo 2) Option 2
+echo 3) Option 3
+echo 4) Option 4
+echo -
+set /p op=Type option:
+if "%op%"=="1" goto op1
+if "%op%"=="2" goto op2
+if "%op%"=="3" goto op3
+if "%op%"=="4" goto op4
+
+echo Please Pick an option:
+goto begin
+
+
+:op1
 call "c:\Users\%Username%\Desktop\BAWR-functions\move-edit-host-file.bat"
 call "c:\Windows\System32\drivers\etc\edit-hosts-file.bat"
 PAUSE
-GOTO End
+goto begin
 
-:Shutdown
-ECHO Shutdown (put your shutdown code here)
-GOTO End
+:op2
+echo you picked option 2
+goto begin
 
-:CloseAllWindows
-ECHO Close All Windows (put your close all windows code here)
-GOTO End
+:op3
+echo you picked option 3
+goto begin
 
-:Logoff
-ECHO Logoff (put your log off code here)
-GOTO End
+:op4
+echo you picked option 4
+goto begin
 
-:SwitchUser
-ECHO Switch User (put your switch user code here)
-GOTO End
-
-
-:End
+:exit
+@exit
