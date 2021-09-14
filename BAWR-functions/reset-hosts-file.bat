341873ed352c4a0512744cc896561a0b7dac6558
rune <christian.rune1998@gmail.com>
2021-09-14 17:44:45 +0200

    Minor fixes

--- BAWR-functions/reset-hosts-file.bat
+++ BAWR-functions/reset-hosts-file.bat
@@ -33,7 +33,7 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 
 :ACCEPTED
 setlocal enabledelayedexpansion
-SET "filename1=%WINDIR%\System32\drivers\etc\hosts"
+SET "filename1=c:\Windows\System32\drivers\etc\\hosts"
 SET "filename1=hosts"
 ::Create your list of host domains
 set LIST=(pornhub.com tv2.dk facebook.com twitter.com instagram.com reddit.com pinterest.com tiktok.com wish.com tinder.com match.com pof.com okcupid.com zoosk.com bumble.com seeking.com happn.com crazygames.com cs-online.club worldofsolitaire.com pogo.com miniclip.com addictinggames.com msn.com foxnews.com buzzfeed.com omegle.com tinychat.com chatroulette.com chatrandom.com bovada.lv betonline.ag lotterypost.com bet365.com flalottery.com lasatlantis.com xvideos.com xnxx.com)
