a91a98a2a0f42ee6bd7ceb492ab0ad902767da04
rune <christian.rune1998@gmail.com>
2021-09-14 12:31:05 +0200

    Update to edit-host-file
    Massive amount of URL's have been added.

--- BAWR-functions/edit-hosts-file.bat
+++ BAWR-functions/edit-hosts-file.bat
@@ -36,12 +36,46 @@ setlocal enabledelayedexpansion
 SET "filename1=%WINDIR%\System32\drivers\etc\hosts"
 SET "filename1=hosts"
 ::Create your list of host domains
-set LIST=(pornhub.com tv2.dk facebook.com twitter.com)
+set LIST=(pornhub.com tv2.dk facebook.com twitter.com instagram.com reddit.com pinterest.com tiktok.com wish.com tinder.com match.com pof.com okcupid.com zoosk.com bumble.com seeking.com happn.com crazygames.com cs-online.club worldofsolitaire.com pogo.com miniclip.com addictinggames.com msn.com foxnews.com buzzfeed.com omegle.com tinychat.com chatroulette.com chatrandom.com bovada.lv betonline.ag lotterypost.com bet365.com flalottery.com lasatlantis.com xvideos.com xnxx.com)
 ::Set the ip of the domains you set in the list above
 set pornhub.com=127.0.0.1
 set tv2.dk=127.0.0.1
 set facebook.com=127.0.0.1
 set twitter.com=127.0.0.1
+set instagram.com=127.0.0.1
+set reddit.com=127.0.0.1
+set pinterest.com=127.0.0.1
+set tiktok.com=127.0.0.1
+set wish.com=127.0.0.1
+set tinder.com=127.0.0.1
+set match.com=127.0.0.1
+set pof.com=127.0.0.1
+set okcupid.com=127.0.0.1
+set zoosk.com=127.0.0.1
+set bumble.com=127.0.0.1
+set seeking.com=127.0.0.1
+set happn.com=127.0.0.1
+set crazygames.com=127.0.0.1
+set cs-online.club=127.0.0.1
+set worldofsolitaire.com=127.0.0.1
+set pogo.com=127.0.0.1
+set miniclip.com=127.0.0.1
+set addictinggames.com=127.0.0.1
+set msn.com=127.0.0.1
+set foxnews.com=127.0.0.1
+set buzzfeed.com=127.0.0.1
+set omegle.com=127.0.0.1
+set tinychat.com=127.0.0.1
+set chatroulette.com=127.0.0.1
+set chatrandom.com=127.0.0.1
+set bovada.lv=127.0.0.1
+set betonline.ag=127.0.0.1
+set lotterypost.com=127.0.0.1
+set bet365.com=127.0.0.1
+set flalottery.com=127.0.0.1
+set lasatlantis.com=127.0.0.1
+set xvideos.com=127.0.0.1
+set xnxx.com=127.0.0.1
 
 ECHO Carrying out requested modifications to your HOSTS file
 :: remove existing names from hosts file
