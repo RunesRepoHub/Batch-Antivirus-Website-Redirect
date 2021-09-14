e7bb9aae8883f92ee80eb5c713d4c7f72af40f78
rune <christian.rune1998@gmail.com>
2021-09-14 14:49:19 +0200

    Update to run bat
    Changed start tag to BAWR to stop errors

--- Run BAWR-V1.0.0.bat
+++ Run BAWR-V1.0.0.bat
@@ -8,7 +8,7 @@ ECHO 5.Switch User
 ECHO 6.Exit
 ECHO.
 
-:Start CHOICE /C 12345 /M "Enter your choice:"
+:BAWR CHOICE /C 12345 /M "Enter your choice:"
 
 :: Note - list ERRORLEVELS in decreasing order
 IF ERRORLEVEL 6 GOTO Exit
@@ -22,23 +22,23 @@ IF ERRORLEVEL 1 GOTO Update-Host-Files
 call "c:\Users\%Username%\Desktop\BAWR-functions\move-edit-host-file.bat"
 call "c:\Windows\System32\drivers\etc\edit-hosts-file.bat"
 PAUSE
-GOTO Start
+GOTO BAWR
 
 :Shutdown
 ECHO Shutdown (put your shutdown code here)
-GOTO Start
+GOTO BAWR
 
 :CloseAllWindows
 ECHO Close All Windows (put your close all windows code here)
-GOTO Start
+GOTO BAWR
 
 :Logoff
 ECHO Logoff (put your log off code here)
-GOTO Start
+GOTO BAWR
 
 :SwitchUser
 ECHO Switch User (put your switch user code here)
-GOTO Start
+GOTO BAWR
 
 
 :Exit
