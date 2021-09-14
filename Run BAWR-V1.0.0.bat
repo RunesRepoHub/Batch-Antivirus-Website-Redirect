874022db1e048e541e554e7390ec4473bf4190e6
rune <christian.rune1998@gmail.com>
2021-09-14 14:51:17 +0200

    Update to run bat
    Fixed names that was giving problems

--- Run BAWR-V1.0.0.bat
+++ Run BAWR-V1.0.0.bat
@@ -5,13 +5,11 @@ ECHO 2.Shutdown
 ECHO 3.Close all Windows
 ECHO 4.Log off
 ECHO 5.Switch User
-ECHO 6.Exit
 ECHO.
 
-:BAWR CHOICE /C 12345 /M "Enter your choice:"
+CHOICE /C 12345 /M "Enter your choice:"
 
 :: Note - list ERRORLEVELS in decreasing order
-IF ERRORLEVEL 6 GOTO Exit
 IF ERRORLEVEL 5 GOTO SwitchUser
 IF ERRORLEVEL 4 GOTO Logoff
 IF ERRORLEVEL 3 GOTO CloseAllWindows
@@ -22,24 +20,23 @@ IF ERRORLEVEL 1 GOTO Update-Host-Files
 call "c:\Users\%Username%\Desktop\BAWR-functions\move-edit-host-file.bat"
 call "c:\Windows\System32\drivers\etc\edit-hosts-file.bat"
 PAUSE
-GOTO BAWR
+GOTO End
 
 :Shutdown
 ECHO Shutdown (put your shutdown code here)
-GOTO BAWR
+GOTO End
 
 :CloseAllWindows
 ECHO Close All Windows (put your close all windows code here)
-GOTO BAWR
+GOTO End
 
 :Logoff
 ECHO Logoff (put your log off code here)
-GOTO BAWR
+GOTO End
 
 :SwitchUser
 ECHO Switch User (put your switch user code here)
-GOTO BAWR
+GOTO End
 
 
-:Exit
-End
+:End
